e8add403c85ff859101e8ba1211f0b8b2631e154
Blitz-Cloud <--help>
2026-06-27 20:41:22 +0300

    Fix ids not being generated automatically by better-auth

--- dbMigrator.py
+++ dbMigrator.py
@@ -1,10 +1,12 @@
 import psycopg2
 from psycopg2.extensions import parse_dsn
 import datetime
+import uuid
+
 
 def migrate_data():
-    source_db = "dbname=rezervariCamin user=admin password=example host=172.27.0.3"
-    target_db = "dbname=Rezervari-Camin user=admin password=example host=172.27.0.3"
+    source_db = "dbname=rezervariCamin user=admin password=example host=172.27.0.2"
+    target_db = "dbname=Rezervari-Camin user=admin password=example host=172.27.0.2"
 
     try:
         src_conn = psycopg2.connect(source_db)
@@ -24,21 +26,56 @@ def migrate_data():
 
         # 2. Iterate and perform multi-table inserts
         for row in rows:
-            useId,usrEmail,usrPassword,usrCreateAt,usrLastSignedIn,usrAuthProvider,usrVerified,usrName,usrProfilePicture,infoUserId,infoUsrPhone,infoUsrCamera = row
+            (
+                usrId,
+                usrEmail,
+                usrPassword,
+                usrCreateAt,
+                usrLastSignedIn,
+                usrAuthProvider,
+                usrVerified,
+                usrName,
+                usrProfilePicture,
+                infoUserId,
+                infoUsrPhone,
+                infoUsrCamera,
+            ) = row
             # print(usrEmail)
             # print(type(usrVerified))
             # # Insert into Table A
             tgt_cur.execute(
                 """INSERT INTO "user" (id,name, email, "emailVerified",image,"updatedAt") VALUES (%s,%s, %s,%s,%s,%s) RETURNING id""",
-                (useId,usrName, usrEmail,usrVerified,usrProfilePicture,datetime.datetime.now())
+                (
+                    usrId,
+                    usrName,
+                    usrEmail,
+                    usrVerified,
+                    usrProfilePicture,
+                    datetime.datetime.now(),
+                ),
             )
             new_user_id = tgt_cur.fetchone()[0]
+            print(new_user_id)
 
-            if infoUsrPhone is not None :
-            # Insert into Table B using the ID from Table A
+            if infoUsrPhone is not None:
+                # Insert into Table B using the ID from Table A
                 tgt_cur.execute(
                     """INSERT INTO  "infoUser"("userId", phone, camera) VALUES (%s, %s, %s)""",
-                    (new_user_id, infoUsrPhone, infoUsrCamera)
+                    (usrId, infoUsrPhone, infoUsrCamera),
+                )
+            if usrAuthProvider == "Email":
+                # Insert into Table B using the ID from Table A
+                tgt_cur.execute(
+                    """INSERT INTO  "account"("accountId", "providerId", password,"createdAt","updatedAt",id,"userId") VALUES (%s,%s,%s,%s,%s,%s,%s)""",
+                    (
+                        usrId,
+                        "credential",
+                        usrPassword,
+                        datetime.datetime.now(),
+                        datetime.datetime.now(),
+                        str(uuid.uuid4()),
+                        usrId,
+                    ),
                 )
 
         tgt_conn.commit()
@@ -50,6 +87,7 @@ def migrate_data():
     finally:
         src_conn.close()
         tgt_conn.close()
+
 
 if __name__ == "__main__":
     migrate_data()
