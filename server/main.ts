01cf5686b51fbe7698e5f8304d438e2f2d5147a6
Blitz-Cloud <--help>
2026-06-27 15:27:34 +0300

    Update stated using the new migrated routes

--- server/main.ts
+++ server/main.ts
@@ -1,20 +1,39 @@
-import  express from "express";
-import * as genezioAdapters from "@genezio/adapters";
-import morgan from "morgan"
-import { BackendService } from "./backend"
+import express from "express";
+import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./auth";
+import { prisma } from "./db";
+import { utilityRouter } from "./routes/utility";
+import { userRouter } from "./routes/user";
+
 const app = express();
 const port = 8881;
-import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 app.use(cors());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use(express.json());
-app.post("/genezio", genezioAdapters.createExpressRouter([BackendService]));
 
-app.listen(port, () => {
+app.use("/api", utilityRouter);
+app.use("/api", userRouter);
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
+app.use(express.json());
+
+const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+const shutDownServer = async () => {
+  server.close(async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", shutDownServer);
+process.on("SIGTERM", shutDownServer);
