85400716085b661ef15b4c399e2d0b85a595613d
blitz-cloud <ionutp626@gmaill.com>
2026-07-13 14:49:07 +0300

    Fix bad port and cors errors

--- server/auth.ts
+++ server/auth.ts
@@ -6,7 +6,7 @@ import bcrypt from "bcrypt";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
-  trustedOrigins: ["http://localhost:5174"],
+  trustedOrigins: ["http://localhost:5173"],
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
