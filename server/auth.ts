4251115c09b8357b8241612c6ae0f4f7d77cba42
blitz-cloud <ionutp626@gmaill.com>
2026-07-13 16:46:43 +0300

    FIx replaced hard coded with envs

--- server/auth.ts
+++ server/auth.ts
@@ -6,7 +6,7 @@ import bcrypt from "bcrypt";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
-  trustedOrigins: ["http://localhost:5173"],
+  trustedOrigins: [process.env.FRONTEND_URL || ""],
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
